b19dd430dd3887f78607651fb0feff0afaf5f5d9
ourgroupwork <ishapun09@gmail.com>
2022-10-19 22:21:53 +0545

    FEATURE:Logdetail mark up

--- frontend/src/screens/LogScreen.js
+++ frontend/src/screens/LogScreen.js
@@ -1,16 +1,37 @@
-import { View, Text, SafeAreaView, Platform } from "react-native";
+import { View, Text, SafeAreaView, Platform, Pressable } from "react-native";
 import React from "react";
 
 // components
 import Nav from "../components/global/Nav";
+import Back from "./../../assets/svg/backArrow.svg";
+import Go from "./../../assets/svg/golo.svg";
 
+import Topnav from "../styles/Log/Topnav";
+import Logdetail from "../styles/Log/Logdetail";
 const LogScreen = ({ navigation }) => {
   return (
     <SafeAreaView
       style={{ paddingTop: Platform.OS === "android" ? 30 : 0, height: "100%" }}
     >
-      <View style={{ flex: 1 }}>
-        <Text>LogScreen</Text>
+      <View style={{ flex: 1, backgroundColor: "#C7CAE9" }}>
+        <View style={Topnav.NavContainer}>
+          <Pressable onPress={() => {}}>
+            <Back />
+          </Pressable>
+          <Text style={Topnav.Text}>Tracking Logs </Text>
+        </View>
+        <View style={Logdetail.Con}>
+          <View style={Logdetail.LeftItem}>
+            <Go />
+            <View>
+              <Text style={Logdetail.Maintxt}>Lumbini Ambulance</Text>
+              <Text style={Logdetail.txt}>Tracked 20 km for 5min</Text>
+            </View>
+          </View>
+          <View style={Logdetail.RightItem}>
+            <Text style={Logdetail.righttxt}>8 jun</Text>
+          </View>
+        </View>
 
         <Nav active={"LogScreen"} navigator={navigation} />
       </View>
---
+++ frontend/src/styles/Log/Logdetail.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+export default StyleSheet.create({
+  Con: {
+    width: "100%",
+    backgroundColor: "#fff",
+    height: 80,
+    padding: 5,
+    flexDirection: "row",
+    alignItems: "center",
+    borderRadius: 15,
+    maarginTop: 4,
+  },
+  LeftItem: {},
+  RightItem: { marginLeft: 20 },
+  Maintxt: {
+    fontFamily: "poppins-m",
+    lineHeight: 21,
+  },
+  txt: {
+    fontFamily: "poppins-l",
+    color: "#919191",
+  },
+  righttxt: {
+    fontFamily: "poppins-r",
+    color: "#686868",
+  },
+});
