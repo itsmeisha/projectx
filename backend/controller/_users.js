d11eaa33ba574a169f527c670fc5707103fc9f8f
Saroj Regmi <sarojregmi.official@gmail.com>
2022-10-29 02:14:29 +0545

    API: auth backend complete

--- backend/controller/_users.js
+++ backend/controller/_users.js
@@ -19,13 +19,17 @@ export const getAllUsers = async (req, res) => {
 };
 
 // checks if the user exists or not in the database
+// if the existance is validated and it is a login request
+// then the user can be logged into the found user
+// means no login function 😅
 export const checkExistance = async (req, res) => {
   // contactInfo is somewhat unique since it is provided by the social auth providers and I believe there are no two accounts with the same google id
   const contactInfo = req?.body?.contact;
-  console.log({
-    data: contactInfo,
-    "req.body": req?.body,
-  });
+  if (!contactInfo)
+    return res.status(400).json({
+      error: "Contact info must be supplied",
+    });
+
   try {
     const user = await userModel.findOne({ contact: contactInfo });
 
@@ -41,6 +45,29 @@ export const checkExistance = async (req, res) => {
   } catch (e) {
     res.status(400).json({
       error: "Unknown error occured while checking the users existance",
+      msg: e,
+    });
+  }
+};
+
+// loging in the user
+export const registerUser = async (req, res) => {
+  const user = req?.body;
+  if (!user)
+    return res.status(400).json({
+      error: "User must be supplied",
+    });
+  try {
+    let registeredUser = new userModel(user);
+
+    // it means the user exists with the given contact information
+    res.status(200).json({
+      msg: "User registered successfully",
+      user: registeredUser,
+    });
+  } catch (e) {
+    res.status(400).json({
+      error: "Unknown error occured while logging user in",
       msg: e,
     });
   }
--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -4,9 +4,14 @@ import express from "express";
 const Router = express.Router();
 
 // getting the controllers
-import { checkExistance, getAllUsers } from "../controller/_users.js";
+import {
+  checkExistance,
+  getAllUsers,
+  registerUser,
+} from "../controller/_users.js";
 
-Router.get("/getallusers", getAllUsers);
-Router.post("/checkexistance", checkExistance);
+Router.get("/getallusers", getAllUsers); // returns all the users in the database
+Router.post("/checkexistance", checkExistance); // used for checking user existance and logging in the user
+Router.post("/register", registerUser); // used to register user
 
 export default Router;
